d50ea8446f7ebbdf0df33e95946c816cdc63cad8
Dan Krod <dan.ga17da@gmail.com>
2025-10-03 18:29:39 +0300

    Add swagger docs to robot_maintenance, sensor

--- app/patrul_robot_project/auth/route/robot_maintenance_route.py
+++ app/patrul_robot_project/auth/route/robot_maintenance_route.py
@@ -5,15 +5,60 @@ robot_maintenance_blueprint = Blueprint('robot_maintenance', __name__)
 
 @robot_maintenance_blueprint.route('/maintenances/<int:robot_id>', methods=['GET'])
 def get_maintenances_for_robot(robot_id):
+    """
+    Get all maintenance records for a specific robot
+    ---
+    tags:
+      - Robot Maintenance
+    parameters:
+      - name: robot_id
+        in: path
+        required: true
+        type: integer
+        description: ID of the robot
+    responses:
+      200:
+        description: List of maintenance records for the robot
+      404:
+        description: Robot not found
+    """
     response, status_code = RobotMaintenanceController.get_maintenances_for_robot(robot_id)
     return jsonify(response), status_code
 
+
 @robot_maintenance_blueprint.route('/maintenances_with_robots', methods=['GET'])
 def get_all_maintenances_with_robots():
+    """
+    Get all maintenance records along with their associated robots
+    ---
+    tags:
+      - Robot Maintenance
+    responses:
+      200:
+        description: List of maintenance records with related robots
+    """
     response, status_code = RobotMaintenanceController.get_all_maintenances_with_robots()
     return jsonify(response), status_code
 
+
 @robot_maintenance_blueprint.route('/robot_maintenance/<int:maintenance_id>', methods=['GET'])
 def get_maintenance_with_robots(maintenance_id):
+    """
+    Get a specific maintenance record along with associated robot information
+    ---
+    tags:
+      - Robot Maintenance
+    parameters:
+      - name: maintenance_id
+        in: path
+        required: true
+        type: integer
+        description: ID of the maintenance record
+    responses:
+      200:
+        description: Maintenance record with associated robot
+      404:
+        description: Maintenance record not found
+    """
     response, status_code = RobotMaintenanceController.get_maintenance_with_robots(maintenance_id)
     return jsonify(response), status_code
--- app/patrul_robot_project/auth/route/sensor_route.py
+++ app/patrul_robot_project/auth/route/sensor_route.py
@@ -5,32 +5,148 @@ sensor_blueprint = Blueprint('sensor', __name__)
 
 @sensor_blueprint.route('/sensors', methods=['POST'])
 def add_sensor():
+    """
+    Add a new sensor
+    ---
+    tags:
+      - Sensor
+    consumes:
+      - application/json
+    parameters:
+      - in: body
+        name: body
+        required: true
+        schema:
+          type: object
+          required:
+            - type
+            - robot_id
+          properties:
+            type:
+              type: string
+              example: "Temperature"
+            robot_id:
+              type: integer
+              example: 1
+    responses:
+      201:
+        description: Sensor created successfully
+      400:
+        description: Invalid input
+    """
     data = request.json
     response, status_code = SensorController.add_sensor(data)
     return jsonify(response), status_code
 
+
 @sensor_blueprint.route('/sensors/<int:sensor_id>', methods=['GET'])
 def get_sensor(sensor_id):
+    """
+    Get a sensor by ID
+    ---
+    tags:
+      - Sensor
+    parameters:
+      - name: sensor_id
+        in: path
+        required: true
+        type: integer
+        description: ID of the sensor
+    responses:
+      200:
+        description: Sensor data
+      404:
+        description: Sensor not found
+    """
     response, status_code = SensorController.get_sensor(sensor_id)
     return jsonify(response), status_code
 
+
 @sensor_blueprint.route('/sensors', methods=['GET'])
 def get_all_sensors():
+    """
+    Get all sensors
+    ---
+    tags:
+      - Sensor
+    responses:
+      200:
+        description: List of all sensors
+    """
     response, status_code = SensorController.get_all_sensors()
     return jsonify(response), status_code
 
+
 @sensor_blueprint.route('/sensors/<int:sensor_id>', methods=['PUT'])
 def update_sensor(sensor_id):
+    """
+    Update a sensor
+    ---
+    tags:
+      - Sensor
+    consumes:
+      - application/json
+    parameters:
+      - name: sensor_id
+        in: path
+        required: true
+        type: integer
+      - in: body
+        name: body
+        required: true
+        schema:
+          type: object
+          properties:
+            type:
+              type: string
+              example: "Humidity"
+            robot_id:
+              type: integer
+              example: 2
+    responses:
+      200:
+        description: Sensor updated successfully
+      404:
+        description: Sensor not found
+    """
     data = request.json
     response, status_code = SensorController.update_sensor(sensor_id, data)
     return jsonify(response), status_code
 
+
 @sensor_blueprint.route('/sensors/<int:sensor_id>', methods=['DELETE'])
 def delete_sensor(sensor_id):
+    """
+    Delete a sensor
+    ---
+    tags:
+      - Sensor
+    parameters:
+      - name: sensor_id
+        in: path
+        required: true
+        type: integer
+        description: ID of the sensor
+    responses:
+      200:
+        description: Sensor deleted successfully
+      404:
+        description: Sensor not found
+    """
     response, status_code = SensorController.delete_sensor(sensor_id)
     return jsonify(response), status_code
 
+
 @sensor_blueprint.route('/sensors_with_robot', methods=['GET'])
 def get_sensors_with_robot():
+    """
+    Get sensors along with their assigned robot
+    ---
+    tags:
+      - Sensor
+    responses:
+      200:
+        description: List of sensors with associated robot
+    """
     response, status_code = SensorController.get_sensors_with_robot()
     return jsonify(response), status_code
